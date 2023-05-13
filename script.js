0d5ab74eb6771cb5b73421b969dbe38287d24136
Pappu Kumar Sharma <100488442+pappukr3631@users.noreply.github.com>
2023-05-13 20:24:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	let arr = ["0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"];
+
+	let ans = [];
+
+	for(let i=0; i<arr[input_digit[0]].length; i++) {
+		for(let j=0; j<arr[input_digit[1]].length; j++) {
+			ans.push(arr[i]+arr[j]);
+		}
+	}
+	ans.sort();
+	return ans;
 }
 
 module.exports = letterCombinations;
