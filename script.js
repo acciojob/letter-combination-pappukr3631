1e4dd6587a21ce93782e57d162138f0a22190992
Pappu Kumar Sharma <100488442+pappukr3631@users.noreply.github.com>
2023-05-13 21:02:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,11 +4,23 @@ function letterCombinations(input_digit) {
 
 	let ans = [];
 //length of input_digit can be more than 2 so below code fails.
-	// for(let i=0; i<arr[input_digit[0]].length; i++) {
-	// 	for(let j=0; j<arr[input_digit[1]].length; j++) {
-	// 		ans.push(arr[input_digit[0]][i]+arr[input_digit[1]][j]);
-	// 	}
-	// }
+	for(let i=0; i<arr[input_digit[0]].length; i++) {
+		for(let j=0; j<arr[input_digit[1]].length; j++) {
+			ans.push(arr[input_digit[0]][i]+arr[input_digit[1]][j]);
+		}
+	}
+	let itr = 2;
+	while(input_digit.length > itr) {
+		let n = ans.length;
+		for(let i=0; i<n; i++)
+			{
+				for(let j=0; j<arr[input_digit[itr]].length; j++)
+					{
+						ans.push(ans[i]+arr[input_digit[itr]][j]);
+					}
+			}
+		itr++;
+	}
 	return ans.sort();
 }
 
